8058c96cde4c1624d5f1d1ef0b9d7f847ab748e7
Yūri Katsuki <yuri@katsuki.moe>
2023-05-23 17:32:22 +0500

    fixed default reply

--- hooks/isReply.ts
+++ hooks/isReply.ts
@@ -1,9 +1,19 @@
-import { Context, NextFunction } from "../deps.ts";
 import { reply } from "../utils/sender.ts";
+import { Context, NextFunction } from "../deps.ts";
+import topics from "../topics.json" assert { type: "json" };
 
 export default async (ctx: Context, next: NextFunction) => {
-  const replyMessage = ctx.message?.reply_to_message;
-  if (!replyMessage) {
+  if (!ctx.message?.is_topic_message) {
+    return await reply(
+      ctx,
+      `⚠️ <b>Biz topicda emasmiz bu komandani ishlatish uchun!</b>`,
+    );
+  }
+
+  if (
+    !ctx.message?.reply_to_message ||
+    Object.values(topics).includes(ctx.message!.reply_to_message!.message_id)
+  ) {
     return await reply(ctx, `↪ Reply bilan ko'rsatingchi habarni!`);
   }
   await next();
