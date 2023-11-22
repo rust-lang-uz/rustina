061f9e62dbe280bde45ad501ed1320b31df22777
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-11-22 23:42:50 +0500

    cargo like logs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,3 +4,14 @@ pub mod inlines;
 pub mod keyboard;
 pub mod message;
 pub mod resources;
+
+pub fn cargo_like_log(title: &str, message: &str) {
+    println!("{}{}{}{} {} {}", 
+        " ".repeat(12 - title.len()),
+        "\x1b[1;32m",
+        title,
+        "\x1b[0m",
+        message,
+        " ".repeat(8)
+    );
+}
